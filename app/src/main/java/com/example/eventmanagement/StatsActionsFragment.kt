a2be5af0a15fad993467560b81c91e26b9234be4
angkykurniawan <kurniawan.angang2005@gmail.com>
2025-12-13 20:55:47 +0700

    Hapus File Event yang Tidak Terpakai

--- app/src/main/java/com/example/eventmanagement/StatsActionsFragment.kt
+++ app/src/main/java/com/example/eventmanagement/StatsActionsFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.activityViewModels
-import com.example.eventmanagement.viewmodels.EventViewModel
 
 class StatsActionsFragment : Fragment() {
 
